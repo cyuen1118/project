8eb922deed7b4906f15e3ac0f7dc1fae31fbd461
cyuen1118 <57658591+cyuen1118@users.noreply.github.com>
2026-03-08 15:33:50 +0800

    Create Minheap.py

---
+++ Minheap.py
@@ -0,0 +1,69 @@
+
+
+
+
+def insert(heap, value):
+    heap.append(value)
+
+    index = len(heap) - 1
+
+
+    while index > 0 and heap[(index - 1) // 2] > heap[index]:
+        heap[index], heap[(index - 1) // 2] = heap[(index - 1) // 2], heap[index]
+
+        index = (index - 1) // 2
+
+
+def deleteMin(heap, value):
+    index = -1
+    for i in range(len(heap)):
+        if heap[i] == value:
+            index = i
+            break
+
+    if index == -1:
+        return
+
+    heap[index] = heap[-1]
+
+    heap.pop()
+
+
+    while True:
+        left_child = 2 * index + 1
+        right_child = 2 * index + 2
+        smallest = index
+
+        if left_child < len(heap) and heap[left_child] < heap[smallest]:
+            smallest = left_child
+        if right_child < len(heap) and heap[right_child] < heap[smallest]:
+            smallest = right_child
+
+        if smallest != index:
+            heap[index], heap[smallest] = heap[smallest], heap[index]
+            index = smallest
+        else:
+            break
+
+
+if __name__ == "__main__":
+    arr = []
+    values = [13, 16, 31, 41, 51, 100]
+    n = len(values)
+
+    for i in range(n):
+        insert(arr, values[i])
+
+    print("Initial heap: ", end="")
+    for val in arr:
+        print(val, end=" ")
+    print()
+
+    deleteMin(arr, 13)
+    print("Heap after deleting 13: ", end="")
+    for val in arr:
+        print(val, end=" ")
+    print()
+    
+    
+    
